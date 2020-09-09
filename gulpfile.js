70cbbea360cf41ebd44903db9e23f1a05d4de67e
lafe <aur@gmx.de>
2020-09-09 15:12:20 +0200

    Added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const {
+    parallel,
+    src,
+    dest
+} = require("gulp");
+const jeditor = require("gulp-json-editor");
+const ts = require("gulp-typescript");
+const tsProject = ts.createProject("tsconfig.json");
+const fs = require('fs');
+const log = require('fancy-log');
+
+const config = {
+    appName: "dev.fernhomberg.streamdeck.homematic",
+};
+
+const rootDistFolder = `dist/${config.appName}`;
+const srcDistFolder = `${rootDistFolder}/src`;
+const assetDistFolder = `${rootDistFolder}/assets`;
+
+function build() {
+    return tsProject
+        .src()
+        .pipe(tsProject())
+        .js
+        .pipe(dest(srcDistFolder));
+}
+
+function copyHtml() {
+    return src("src/**/*.html")
+        .pipe(dest(rootDistFolder));
+}
+
+function copyManifest() {
+    return src("./manifest.json")
+        .pipe(jeditor((json)=> {
+            const packageDefinition = JSON.parse(fs.readFileSync("./package.json"));
+            json.Version = packageDefinition.version;
+            log.info(`Updated version information in manifest to ${packageDefinition.version}`);
+            return json;
+        }))
+        .pipe(dest(rootDistFolder));
+}
+
+function copyAssets() {
+    return src("assets/**/*.png")
+        .pipe(dest(assetDistFolder));
+}
+
+exports.default = parallel(build, copyHtml, copyManifest, copyAssets);
