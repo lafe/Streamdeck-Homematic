26c801307efea52f0efd37d120420d4483c0bbf6
lafe <aur@gmx.de>
2020-09-11 19:12:25 +0200

    Loading/Storing settings and loading devices

--- src/propertyInspector/RelayComponent.tsx
+++ src/propertyInspector/RelayComponent.tsx
@@ -1,37 +1,106 @@
 import React, { useEffect, useState } from "react";
 import { getLogger } from "../common/Logger";
 import { PropertyInspectorContainer } from "../components/PropertyInspectorContainer/PropertyInspectorContainer";
+import { Spinner } from "../components/Spinner/Spinner";
 import { TextBox } from "../components/TextBox/TextBox";
 import { Device } from "../data/Device";
+import { RelaySettings } from "../data/settings/RelaySettings";
 import { loadDevices } from "../homematic/loadDevices";
+import { useStreamDeck } from "../streamdeck/useStreamDeck";
 
 export function RelayComponent() {
-    const [ip, setIp] = useState<string | undefined>(undefined);
+    const [isDevicesLoading, setIsDevicesLoading] = useState(false);
     const [devices, setDevices] = useState<Device[]>([]);
+    const [settings, setSettings] = useState<RelaySettings | undefined>(undefined);
+    const [isSettingsLoading, setIsSettingsLoading] = useState(false);
+    const [streamdeckConnected, setStreamdeckConnected] = useState(false);
+
     const logger = getLogger("RelayComponent");
+    const streamdeck = useStreamDeck();
+
+    useEffect(() => {
+        if (streamdeck.isConnected) {
+            logger.info("StreamDeck connected");
+            setStreamdeckConnected(true);
+            return;
+        }
+        logger.warn("StreamDeck not yet connected");
+        let timerId: number | undefined = undefined;
+        timerId = window.setInterval(() => {
+            if (!streamdeck.isConnected) {
+                logger.warn("StreamDeck still not connected.");
+            } else {
+                logger.info("StreamDeck connected.");
+                setStreamdeckConnected(true);
+            }
+        }, 200);
+
+        return () => { window.clearInterval(timerId); };
+    }, [streamdeckConnected]);
 
     useEffect(() => {
         let didCancel = false;
         async function fetchDevices() {
-            const fetchedDevices = ip == null ? [] : await loadDevices(ip);
+            setIsDevicesLoading(true);
+            const fetchedDevices = settings?.address == null ? [] : await loadDevices(settings.address);
             if (!didCancel) {
+                logger.log("Retrieved devices", fetchedDevices);
                 setDevices(fetchedDevices);
+                setIsDevicesLoading(false);
             }
         }
         fetchDevices();
-
         return () => { didCancel = true; };
-    }, [ip]);
+    }, [settings?.address]);
 
-    const onIpChange = async (newIp?: string) => {
-        logger.log(`Handling new IP ${newIp}`);
-        setIp(newIp);
+    useEffect(() => {
+        let didCancel = false;
+        async function loadSettings() {
+            if (!streamdeck.isConnected) {
+                return;
+            }
+            setIsSettingsLoading(true);
+            const loadedSettings = await streamdeck.getSettings<RelaySettings>();
+            if (!didCancel) {
+                logger.log("Retrieved settings", loadedSettings);
+                setSettings(loadedSettings);
+                setIsSettingsLoading(false);
+            }
+        }
+
+        if (settings == null) {
+            logger.info("Settings empty. Loading settings.");
+            if (!streamdeck.isConnected) {
+                logger.warn("StreamDeck not yet connected");
+            } else {
+                loadSettings();
+            }
+        }
+
+        return () => {
+            didCancel = true;
+        };
+    }, [streamdeckConnected]);
+
+    const onIpChange = async (newAddress?: string) => {
+        logger.log(`Handling new address ${newAddress}`);
+        const newSettings = settings == null ? {} as RelaySettings : { ...settings };
+        newSettings.address = newAddress ?? "";
+        streamdeck.setSettings(newSettings);
+        setSettings(newSettings);
+        
     };
+
+    if (settings == null || isSettingsLoading) {
+        return <Spinner />;
+    }
 
     return (
         <PropertyInspectorContainer>
-            <TextBox label="HomeMatic IP" onChange={(newIp) => onIpChange(newIp)} />
-            {devices.map((device, index) => <div key={`device-${device.name}-${index}`}>{device.name}</div> )}
+            <TextBox label="Address" placeholder="Address of Homematic CCU" defaultValue={settings.address} onChange={(newAddress) => onIpChange(newAddress)} />
+            {isDevicesLoading && <Spinner />}
+            {settings?.address}
+            {devices.map((device, index) => <div key={`device-${device.name}-${index}`}>{device.name}</div>)}
         </PropertyInspectorContainer>
     );
 }
--- src/streamdeck/StreamDeck.ts
+++ src/streamdeck/StreamDeck.ts
@@ -1,8 +1,13 @@
-import { debugLog } from "../common/debugLog";
+import { v4 as uuidv4 } from "uuid";
 import { getWebSocketError } from "../common/getWebSocketError";
 import { getLogger, Logger } from "../common/Logger";
 import { parseJson } from "../common/parseJson";
+import { Dictionary } from "../data/Dictionary";
 import { BaseMessage } from "../message/BaseMessage";
+import { ReceiveSettingsMessage } from "../message/ReceiveSettingsMessage";
+import { RegisterPropertyInspectorMessage } from "../message/RegisterPropertyInspectorMessage";
+
+export type StreamDeckIncomingMessageHandler = (instance: StreamDeck, message: BaseMessage) => void;
 
 export class StreamDeck {
     private static instance: StreamDeck;
@@ -13,29 +18,40 @@ export class StreamDeck {
         return StreamDeck.instance;
     }
 
-    public incomingMessage?: (instance: StreamDeck, message: BaseMessage) => void = undefined;
+    protected incomingMessageHandlers: Dictionary<StreamDeckIncomingMessageHandler> = {};
 
     protected inPort?: string;
     protected inUUID?: string;
     protected inMessageType?: string;
     protected inApplicationInfo?: string;
     protected inActionInfo?: string;
-    protected websocket?: WebSocket;
+    protected device?: string;
+    protected context?: string;
 
+    protected websocket?: WebSocket;
     protected Logger: Logger;
+    public isConnected = false;
 
     constructor() {
         this.Logger = getLogger("StreamDeck");
     }
 
+    public registerHandler(id: string, handler: StreamDeckIncomingMessageHandler) {
+        this.incomingMessageHandlers[id] = handler;
+    }
+    public removeHandler(id: string) {
+        delete this.incomingMessageHandlers[id];
+    }
+
     protected showVars(): void {
-        debugLog("---- showVars");
-        debugLog("- port", this.inPort);
-        debugLog("- uuid", this.inUUID);
-        debugLog("- messagetype", this.inMessageType);
-        debugLog("- info", this.inApplicationInfo);
-        debugLog("- inActionInfo", this.inActionInfo);
-        debugLog("----< showVars");
+        this.Logger.log("Connection variables:");
+        this.Logger.log(`- Port: ${this.inPort}`);
+        this.Logger.log(`- UUID: ${this.inUUID}`);
+        this.Logger.log(`- Messagetype: ${this.inMessageType}`);
+        this.Logger.log(`- Info: ${this.inApplicationInfo}`);
+        this.Logger.log(`- InActionInfo: ${this.inActionInfo}`);
+        this.Logger.log(`- Device: ${this.device}`);
+        this.Logger.log(`- Context: ${this.context}`);
     }
 
     public connect(inPort: string, inUUID: string, inMessageType: string, inApplicationInfo: string, inActionInfo: string): void {
@@ -45,10 +61,13 @@ export class StreamDeck {
         this.inApplicationInfo = inApplicationInfo;
         this.inActionInfo = inActionInfo;
 
-        /** Debug variables */
-        if (window.debug) {
-            this.showVars();
+        if (this.inMessageType === "registerPropertyInspector" && this.inActionInfo != null) {
+            const message = JSON.parse(this.inActionInfo) as RegisterPropertyInspectorMessage<unknown>;
+            this.device = message.device;
+            this.context = message.context;
         }
+
+        this.showVars();
 
         // const lang = Utils.getProp(inApplicationInfo, "application.language", false);
         // if (lang) {
@@ -61,6 +80,7 @@ export class StreamDeck {
         if (this.websocket != null) {
             this.websocket.close();
             this.websocket = undefined;
+            this.isConnected = false;
         }
 
         this.websocket = new WebSocket("ws://127.0.0.1:" + inPort); //localhost
@@ -69,6 +89,7 @@ export class StreamDeck {
         this.websocket.onerror = (evt) => this.websocketOnError(evt);
         this.websocket.onclose = (evt) => this.websocketOnClose(evt);
         this.websocket.onmessage = (evt) => this.websocketOnMessageReceived(evt);
+        this.isConnected = true;
     }
 
     protected websocketOnOpen(): void {
@@ -89,10 +110,11 @@ export class StreamDeck {
     protected websocketOnError(evt: Event): void {
         this.Logger.warn("WEBOCKET ERROR", evt);
     }
-    
+
     protected websocketOnClose(evt: CloseEvent): void {
         const reason = getWebSocketError(evt);
         this.Logger.warn(`WEBOCKET CLOSED. Reason: ${reason}`);
+        this.isConnected = false;
     }
 
     protected websocketOnMessageReceived(evt: MessageEvent): void {
@@ -104,18 +126,65 @@ export class StreamDeck {
         }
 
         this.Logger.log(`Received message ${payloadMessage.event}`, evt, payloadMessage);
-        if(this.incomingMessage == null){
-            this.Logger.warn("Cannot process message, because handler is empty");
-            return;
+        for (const incomingMessageHandlerId of Object.keys(this.incomingMessageHandlers)) {
+            try {
+                const incomingMessageHandler = this.incomingMessageHandlers[incomingMessageHandlerId];
+                incomingMessageHandler(this, payloadMessage);
+            } catch (e) {
+                this.Logger.error(`While trying to invoke handler "${incomingMessageHandlerId}", an error occurred`, e);
+            }
         }
-        this.incomingMessage(this, payloadMessage);
     }
 
-    public sendJson(data: unknown): void {
+    private buildMessage<TPayload>(type: "setSettings" | "getSettings" | "logMessage", payload?: TPayload) {
+        const message: { event: string, context?: string, payload?: TPayload } = { event: type };
+        message.context = this.inUUID;
+        if (payload != null) {
+            message.payload = payload;
+        }
+        return message;
+    }
+
+    public getSettings<TSettings>(): Promise<TSettings> {
+        this.Logger.log("Retrieving settings");
+        const promise = new Promise<TSettings>(resolve => {
+            try {
+                const handlerId = `internalGetSettingsHandler-${uuidv4()}`;
+                const message = this.buildMessage("getSettings");
+
+                const handleSettingsRetrieval = (streamDeck: StreamDeck, message: BaseMessage) => {
+                    if (message.event !== "didReceiveSettings") {
+                        return;
+                    }
+                    const settingsMessage = message as ReceiveSettingsMessage<TSettings>;
+                    resolve(settingsMessage.payload.settings);
+                    this.removeHandler(handlerId);
+                };
+                this.registerHandler(handlerId, handleSettingsRetrieval);
+                this.sendJson(message);
+            }
+            catch (e) {
+                this.Logger.error("While retrieving the settings, an error occurred", e);
+            }
+        });
+        return promise;
+    }
+    public setSettings<TSettings>(settings: TSettings): void {
+        this.Logger.log("Setting settings");
+        const message = this.buildMessage("setSettings", settings);
+        this.sendJson(message);
+    }
+
+    protected sendJson(data: unknown): void {
         if (this.websocket == null) {
             this.Logger.error("Cannot send paylod via websocket, because the websocket is null.", data);
             return;
         }
+        if (data == null) {
+            this.Logger.warn("Cannot send empty payload to Streamdeck");
+            return;
+        }
+
         const payload = JSON.stringify(data);
         this.Logger.info("Sending data to Streamdeck", payload);
         this.websocket.send(payload);
