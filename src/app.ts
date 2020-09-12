ef4aea983197b29cc9d4452352addb85c506c0c3
lafe <aur@gmx.de>
2020-09-12 16:33:23 +0200

    Commented SDK code

--- src/app.ts
+++ src/app.ts
@@ -26,39 +26,37 @@ declare global {
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function connectElgatoStreamDeckSocket(inPort: string, inUUID: string, inMessageType: string, inApplicationInfo: string, inActionInfo: string) {
-    // window.$SD.api = Object.assign({ send: SDApi.send }, SDApi.common, SDApi[inMessageType]);
-    console.log("Hello World");
-    console.log("Localized Strings", window.$localizedStrings);
-    console.log("Remote Settings", window.REMOTESETTINGS);
-    console.log("isQt", isQT);
-    console.log("debug", window.debug);
-    console.log("debug log", debugLog);
-    console.log("MIMAGECACHE", window.MIMAGECACHE);
+    // console.log("Localized Strings", window.$localizedStrings);
+    // console.log("Remote Settings", window.REMOTESETTINGS);
+    // console.log("isQt", isQT);
+    // console.log("debug", window.debug);
+    // console.log("debug log", debugLog);
+    // console.log("MIMAGECACHE", window.MIMAGECACHE);
     // eslint-disable-next-line prefer-rest-params
     const streamdeck = StreamDeck.getInstance();
     streamdeck.connect(inPort, inUUID, inMessageType, inApplicationInfo, inActionInfo);
     new StreamDeckHandler(streamdeck);
 }
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable no-var */
-window.$localizedStrings = window.$localizedStrings || {};
-window.REMOTESETTINGS = window.REMOTESETTINGS || {};
-var DestinationEnum = Object.freeze({
-        HARDWARE_AND_SOFTWARE: 0,
-        HARDWARE_ONLY: 1,
-        SOFTWARE_ONLY: 2,
-    });
-var isQT = navigator.appVersion.includes("QtWebEngine");
-window.debug = window.debug ?? false;
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-var debugLog: (messate: string, ...data: any[]) => void = function() {};
-window.MIMAGECACHE = window.MIMAGECACHE || {};
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// /* eslint-disable no-var */
+// window.$localizedStrings = window.$localizedStrings || {};
+// window.REMOTESETTINGS = window.REMOTESETTINGS || {};
+// var DestinationEnum = Object.freeze({
+//         HARDWARE_AND_SOFTWARE: 0,
+//         HARDWARE_ONLY: 1,
+//         SOFTWARE_ONLY: 2,
+//     });
+// var isQT = navigator.appVersion.includes("QtWebEngine");
+// window.debug = window.debug ?? false;
+// // eslint-disable-next-line @typescript-eslint/no-empty-function
+// var debugLog: (messate: string, ...data: any[]) => void = function() {};
+// window.MIMAGECACHE = window.MIMAGECACHE || {};
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const setDebugOutput = (debug: any) => (debug === true ? console.log.bind(window.console) : function() {});
-debugLog = setDebugOutput(window.debug);
+// // eslint-disable-next-line @typescript-eslint/no-empty-function
+// const setDebugOutput = (debug: any) => (debug === true ? console.log.bind(window.console) : function() {});
+// debugLog = setDebugOutput(window.debug);
 
 
 window.connectElgatoStreamDeckSocket = (inPort: string, inUUID: string, inMessageType: string, inApplicationInfo: string, inActionInfo: string) => connectElgatoStreamDeckSocket(inPort, inUUID, inMessageType, inApplicationInfo, inActionInfo);
